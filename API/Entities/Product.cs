ad74b8c0e526eeee589222f1d9c30dce64dbe1b7
Imon Islam <imon0077@yahoo.com>
2023-10-27 15:37:59 +0600

    run migration for products & setup basic endpoints for products

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -3,6 +3,6 @@ namespace API.Entities
     public class Product
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
     }
 }
