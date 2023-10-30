a18d624b8c4f40a22d8d53a55383113f8ffda0d5
Imon Islam <imon0077@yahoo.com>
2023-10-30 23:27:05 +0600

    Repositor pattern implmented for Products, BaseEntity class added for common columns, related model class added for product

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
