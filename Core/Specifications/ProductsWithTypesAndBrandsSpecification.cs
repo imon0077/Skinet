f469b6b8fc049156fc71b8abe97dfd1692ef31ec
Imon Islam <imon0077@yahoo.com>
2023-12-02 11:24:40 +0600

    Pagination added for the product with Specification Pattern

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -5,18 +5,20 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification(string sort, int? branId, int? typeId)
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
             : base(x => 
-                (!branId.HasValue || x.ProductBrandId == branId) && 
-                (!typeId.HasValue || x.ProductTypeId == typeId)
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
             AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), 
+            productParams.PageSize);
 
-            if(!string.IsNullOrEmpty(sort)){
-                switch (sort)
+            if(!string.IsNullOrEmpty(productParams.Sort)){
+                switch (productParams.Sort)
                 {
                     case "priceAsc":
                         AddOrderBy(p => p.Price);
