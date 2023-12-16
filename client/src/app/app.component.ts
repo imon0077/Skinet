ecf1022db4987f3a00c589c80357a343aa66b139
Imon Islam <imon0077@yahoo.com>
2023-12-16 12:28:19 +0600

    font-awesome installation bug fixed, navbar added along with some styling in client-app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
+  //standalone: true,
+  //imports: [FontAwesomeModule], // alternatively, individual components can be imported
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -10,5 +14,6 @@ export class AppComponent {
   constructor() {
     setTheme('bs5'); // or 'bs4'
   }
+  //faCoffee = faCoffee;
   title = 'Skinet';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,19 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     TooltipModule.forRoot(),
-    FontAwesomeModule
+    //FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  //faCoffee = faCoffee;
+}
